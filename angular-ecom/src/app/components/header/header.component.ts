ea40101efca84a020507bbfbc346c3268386fdcd
I A Santha Ashwin <santhaashwin24@gmail.com>
2025-04-01 07:18:44 +0000

    first commit

---
+++ angular-ecom/src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <div class="bg-slate-100 px-4 py-3 text-bold">
+      header works!
+    </div>
+  `,
+  styles: ``
+})
+export class HeaderComponent {
+
+}
